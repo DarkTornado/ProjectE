60aa8128e21a206395fbad29d419a581a3baec39
Dark Tornado <dt3141592@naver.com>
2021-10-11 18:49:26 +0900

    깃허브 링크로 변경

--- js/script.js
+++ js/script.js
@@ -6,3 +6,8 @@ function gotoGithub() {
     alert("구버전은 깃허브에서 확인이 가능합니다.");
     location.href = "https://github.com/DarkTornado/ProjectE/blob/master/README.md#배포용-버전-목록";
 }
+
+function cmdList() {
+    alert("명렁어 목록은 깃허브에서 확인이 가능합니다.");
+    location.href = "https://github.com/DarkTornado/ProjectE/blob/master/README.md#명령어-목록";
+}
